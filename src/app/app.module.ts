1931c773559b767f2d2d0cec19de9614b1beea6a
wwz-PC\wwz <wangwanzhong@sphinxjoy.com>
2017-03-24 19:56:13 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu.component';
+import { WeatherComponent } from './weather/weather.component';
+import { CurrencyComponent } from './currency/currency.component';
+import { MovieComponent } from './movie/movie.component';
+import { CONST_ROUTING } from './app.routing'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    WeatherComponent,
+    CurrencyComponent,
+    MovieComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    CONST_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
